f2fd8bfb2a7b65461214735873a36eef85012a22
halamlee <halam.lee.lami@gmail.com>
2022-11-10 12:41:26 +0900

    :lipstick: (halam/setting/#1) 레이아웃 생성

---
+++ money-for-rabbit/src/styles/layout/Layout.jsx
@@ -0,0 +1,16 @@
+/** @jsxImportSource @emotion/react */
+
+import React from 'react';
+import { css } from '@emotion/react';
+
+function Layout({ page }) {
+  return <div css={wrapper}>{page}</div>;
+}
+
+export default Layout;
+
+const wrapper = css`
+  width: 390px;
+  height: 100vh;
+  background-color: blue;
+`;
