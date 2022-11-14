5a73c96233dc9607188f868e2caa169928ddf0d9
taeyoung <rlaxodud911@gmail.com>
2022-11-15 00:26:56 +0900

    :sparkles: (taeyoung/#8) 텍스트 버튼 컴포넌트 생성

---
+++ money-for-rabbit/src/components/button/TextButton.jsx
@@ -0,0 +1,46 @@
+/** @jsxImportSource @emotion/react */
+
+import React from 'react';
+import { css } from '@emotion/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+
+function TextButton({ label, onClick }) {
+  return label === '이전' ? (
+    <PrevButton onClick={onClick} />
+  ) : (
+    <MoreButton onClick={onClick} />
+  );
+}
+
+export default TextButton;
+
+const textButtonBase = css`
+  font-weight: 600;
+  cursor: pointer;
+  user-select: none;
+`;
+
+const prev = css`
+  ${textButtonBase}
+  color: #c39d52;
+  font-size: 24px;
+`;
+
+const more = css`
+  ${textButtonBase}
+  color: #ecd189;
+  font-size: 16px;
+`;
+
+const PrevButton = (props) => (
+  <div css={prev} {...props}>
+    <FontAwesomeIcon icon={faAngleLeft} /> 이전
+  </div>
+);
+
+const MoreButton = (props) => (
+  <div css={more} {...props}>
+    더보기 <FontAwesomeIcon icon={faAngleRight} />
+  </div>
+);
