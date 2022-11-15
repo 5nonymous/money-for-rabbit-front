9935539b6e303e3e7751ab6c22e14a1d1c48a1d3
taeyoung <rlaxodud911@gmail.com>
2022-11-15 20:59:25 +0900

    :sparkles: (taeyoung/#15) NotFound 페이지 링크 추가

--- money-for-rabbit/src/App.jsx
+++ money-for-rabbit/src/App.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './styles/layout/Layout';
 import { css } from '@emotion/react';
 import Home from './pages/home/Home';
+import NotFound from './pages/error/NotFound';
 
 function App() {
   return (
     <div css={wrapper}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout page={<Home />} />} />
+          <Route path='/' element={<Layout page={<Home />} />} />
+          <Route path='/*' element={<Layout page={<NotFound />} />} />
         </Routes>
       </BrowserRouter>
     </div>
