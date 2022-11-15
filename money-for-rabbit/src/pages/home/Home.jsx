376ba742473fb7492802b1f11d9d6d7467cb98f6
halamlee <halam.lee.lami@gmail.com>
2022-11-15 12:03:11 +0900

    :sparkles::lipstick: (halam/#5) 시간에 따른 배경이미지 변화

--- money-for-rabbit/src/pages/home/Home.jsx
+++ money-for-rabbit/src/pages/home/Home.jsx
@@ -3,6 +3,31 @@
 import React from 'react';
 import { css } from '@emotion/react';
 
+const getHours = () => {
+  // const now = new Date();
+  // const hours = now.getHours();
+
+  const test = new Date(2022, 11, 15, 14, 39, 20);
+  const hours = test.getHours();
+  return hours;
+};
+
+const getBackgroundImage = (hours) => {
+  // 07 ~ 16 : afternoon
+  // 17 ~ 18 : dusk
+  // 19 ~ 06 : night
+
+  if (7 <= hours && hours <= 16) {
+    return './images/Background:Afternoon.jpeg';
+  } else if (17 <= hours && hours <= 18) {
+    return './images/Background:Dusk.jpeg';
+  } else {
+    return './images/Background:Night.jpeg';
+  }
+};
+
+const backgroundImage = getBackgroundImage(getHours());
+
 function Home() {
   return <div css={wrapper} />;
 }
@@ -12,5 +37,5 @@ export default Home;
 const wrapper = css`
   width: 100%;
   height: 100%;
-  background: url('./images/Background:Afternoon.jpeg') center/cover;
+  background: url(${backgroundImage}) center/cover;
 `;
