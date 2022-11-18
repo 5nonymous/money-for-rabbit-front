c1e1fc8e945525b40e5a0121b9e5f18e5c21f28b
halamlee <halam.lee.lami@gmail.com>
2022-11-19 01:05:06 +0900

    :card_file_box: (halam/#41) 쪽지목록 더미데이터 생성

---
+++ money-for-rabbit/src/pages/letter/dummyData.js
@@ -0,0 +1,10 @@
+const data = [
+  { writer: '나다짜식아', image: 'Money_100.png' },
+  { writer: '나다짜식아', image: 'Money_100.png' },
+  { writer: '나다짜식아', image: 'Money_100.png' },
+  { writer: '나다짜식아', image: 'Money_100.png' },
+  { writer: '나다짜식아', image: 'Money_100.png' },
+  { writer: '나다짜식아', image: 'Money_100.png' },
+];
+
+export default data;
