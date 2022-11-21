283d43dc12dc423688e39e30956405201352cf4c
taeyoung <rlaxodud911@gmail.com>
2022-11-21 22:18:22 +0900

    :construction: (taeyoung/#43) 이메일 인증 페이지 퍼블리싱

---
+++ money-for-rabbit/src/pages/sign-up/EmailAuthentication.jsx
@@ -0,0 +1,107 @@
+/** @jsxImportSource @emotion/react */
+
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { css } from '@emotion/react';
+import common from '../../styles/common';
+import BoxButton from '../../components/button/BoxButton';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+function EmailAuthentication() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [userData, setUserData] = useState();
+
+  const resendEmail = () => {
+    alert('이메일을 재발송 합니다.');
+  };
+
+  useEffect(() => {
+    if (!location.state) {
+      navigate('/signup');
+    } else {
+      setUserData(location.state.data);
+    }
+  }, [navigate, location]);
+
+  if (userData) {
+    return (
+      <div css={wrapper}>
+        <h1>
+          {userData.nickname} 님,
+          <br />
+          회원가입을 환영합니다!
+        </h1>
+
+        <p>
+          <span>{userData.email}</span> 으로
+          <br />
+          인증 메일을 발송하였습니다.
+        </p>
+
+        <div>
+          <p>
+            메일함 확인 후
+            <br />
+            이메일 인증을 진행해주세요.
+          </p>
+          <p>
+            받지 못하셨나요? <span onClick={() => resendEmail()}>메일 재발송</span>
+          </p>
+        </div>
+
+        <BoxButton onClick={() => navigate('/')}>메인으로 가기</BoxButton>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
+}
+
+export default EmailAuthentication;
+
+const wrapper = css`
+  width: 100%;
+  height: 100%;
+
+  ${common.align.centerColumn}
+  gap: 49px;
+
+  color: ${common.color.brown4};
+  text-align: center;
+
+  user-select: none;
+
+  h1 {
+    ${common.fontSize[30]}
+    ${common.fontWeight.bold}
+  }
+
+  p {
+    ${common.fontSize[20]}
+    ${common.fontWeight.medium}
+    line-height: 35px;
+  }
+
+  span {
+    ${common.fontWeight.bold}
+    user-select: text;
+  }
+
+  div {
+    p:last-child {
+      ${common.fontSize[16]}
+      line-height: 25px;
+    }
+
+    span {
+      cursor: pointer;
+      user-select: none;
+
+      :hover {
+        border-bottom: 1px solid ${common.color.brown4};
+      }
+    }
+  }
+`;
