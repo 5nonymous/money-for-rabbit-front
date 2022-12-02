1a63ddd2b0d2f83f6ba4e4f2754f1f3853f814bb
halamlee <halam.lee.lami@gmail.com>
2022-12-03 03:58:10 +0900

    :card_file_box: (halam/#41) 쪽지목록 더미데이터 아이디 항목 추가, 이미지 종류 다양하게 데이터 추가

--- money-for-rabbit/src/pages/letter/dummyData.js
+++ money-for-rabbit/src/pages/letter/dummyData.js
@@ -1,10 +1,10 @@
 const data = [
-  { writer: '나다짜식아', image: 'Money_100.png' },
-  { writer: '나다짜식아', image: 'Money_100.png' },
-  { writer: '나다짜식아', image: 'Money_100.png' },
-  { writer: '나다짜식아', image: 'Money_100.png' },
-  { writer: '나다짜식아', image: 'Money_100.png' },
-  { writer: '나다짜식아', image: 'Money_100.png' },
+  { id: 1, writer: '나다짜식아', image: 'Money_100.png' },
+  { id: 2, writer: '나다짜식아', image: 'Money_500.png' },
+  { id: 3, writer: '나다짜식아', image: 'Money_1000.png' },
+  { id: 4, writer: '나다짜식아', image: 'Money_5000.png' },
+  { id: 5, writer: '나다짜식아', image: 'Money_10000.png' },
+  { id: 6, writer: '나다짜식아', image: 'Money_50000.png' },
 ];
 
 export default data;
