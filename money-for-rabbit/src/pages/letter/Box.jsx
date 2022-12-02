0a5bbcdaeb640d7d251a38705c6caf486f284437
halamlee <halam.lee.lami@gmail.com>
2022-12-03 04:04:35 +0900

    :sparkles::lipstick: (halam/#41) 잘못 사용한 css 함수 적용 방법 수정과 금액에 따라 다르게 적용하는 스타일 추가

--- money-for-rabbit/src/pages/letter/Box.jsx
+++ money-for-rabbit/src/pages/letter/Box.jsx
@@ -1,16 +1,17 @@
-// TODO: 금액별로 다른 이미지, 위치, 사이즈 주도록 수정해야 함 => 디자인이 나와야 작업 가능
-
 /** @jsxImportSource @emotion/react */
 
 import React from 'react';
 import { css } from '@emotion/react';
 import common from '../../styles/common';
 import TextButton from '../../components/button/TextButton';
+import { getMoneyImage } from '../../utils/getDynamicImage';
 
 function Box({ size, writer, priceImg }) {
+  console.log('getMoneyImage(priceImg)', getMoneyImage(priceImg));
+
   return (
-    <div css={wrapper}>
-      <div css={priceImgWrapper}>{priceImg}</div>
+    <div css={wrapper(size)}>
+      <div css={priceImgWrapper(priceImg)} />
       {writer} 님
       <div css={textButtonWrapper}>
         <TextButton label={'더보기'} />
@@ -21,9 +22,9 @@ function Box({ size, writer, priceImg }) {
 
 export default Box;
 
-const wrapper = (props) => css`
-  width: ${props.size === 'big' ? '319px' : '150px'};
-  height: ${props.size === 'big' ? '363px' : '130px'};
+const wrapper = (size) => css`
+  width: ${size === 'big' ? '319px' : '150px'};
+  height: ${size === 'big' ? '363px' : '130px'};
   border-radius: 11px;
   border: 1px solid ${common.color.brown4};
   background-color: ${common.color.brown1};
@@ -36,12 +37,8 @@ const wrapper = (props) => css`
   position: relative;
 `;
 
-const priceImgWrapper = css`
-  position: absolute;
-  top: calc(46px / -2);
-  width: 47px;
-  height: 46px;
-  background: url('/images/Money_100.png') center/cover no-repeat;
+const priceImgWrapper = (priceImg) => css`
+  ${getMoneyImage(priceImg)};
 `;
 
 const textButtonWrapper = css`
