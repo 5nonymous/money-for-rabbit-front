8cbfafdbff0690538c3c2e735b588d417bc9d5db
halamlee <halam.lee.lami@gmail.com>
2022-12-09 14:26:47 +0900

    :sparkles: (halam/#42) 쪽지 상세조회 완성 - 캡쳐, 공유 기능 가능

--- money-for-rabbit/src/pages/letter/Letter.jsx
+++ money-for-rabbit/src/pages/letter/Letter.jsx
@@ -1,22 +1,46 @@
 /** @jsxImportSource @emotion/react */
 
 import React from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { css } from '@emotion/react';
 import common from '../../styles/common';
 import TextButton from '../../components/button/TextButton';
 import { letterData as dummyData } from './dummyData';
 import Box from './Box';
-import BoxButton from '../../components/button/BoxButton';
+import IconButton from '../../components/button/IconButton';
+import html2canvas from 'html2canvas';
 
 function Letter() {
   const navigate = useNavigate();
 
   const userName = '어쩌구';
 
+  const onClickCaptureBtn = () => {
+    document.getElementById('prevBtn').style.opacity = 0;
+    html2canvas(document.getElementById('container')).then((canvas) => {
+      onSave(canvas.toDataURL('image/png'), 'money-for-rabbit-download.png');
+    });
+    document.getElementById('prevBtn').style.opacity = 1;
+
+    const onSave = (uri, filename) => {
+      const link = document.createElement('a');
+      document.body.appendChild(link);
+      link.href = uri;
+      link.download = filename;
+      link.click();
+      document.body.removeChild(link);
+    };
+  };
+
+  const onClickShareBtn = () => {
+    navigator.clipboard.writeText(window.location.href).then(() => {
+      alert('링크 복사 완료');
+    });
+  };
+
   return (
     <div css={wrapper}>
-      <div css={textButtonWrapper}>
+      <div css={textButtonWrapper} id="prevBtn">
         <TextButton label={'이전'} onClick={() => navigate(-1)} />
       </div>
       <h1>{userName} 님이 받은 세뱃돈 입니다.</h1>
@@ -27,6 +51,10 @@ function Letter() {
           contents={dummyData.message}
           priceImg={dummyData.image_name}
         />
+      </div>
+      <div css={btnWrapper}>
+        <IconButton capture onClick={onClickCaptureBtn} />
+        <IconButton share onClick={onClickShareBtn} />
       </div>
     </div>
   );
@@ -62,4 +90,13 @@ const lettersWrapper = css`
   > div:not(:nth-of-type(4) ~ div) {
     margin-bottom: 47px;
   }
+`;
+
+const btnWrapper = css`
+  width: 175px;
+  display: flex;
+  gap: 37px;
+  position: absolute;
+  bottom: 53px;
+  right: 36px;
 `;
