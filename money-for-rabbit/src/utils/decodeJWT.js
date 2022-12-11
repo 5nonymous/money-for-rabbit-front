2ee71917f3a86e7d9e6aff402af7e755eaf49abb
taeyoung <rlaxodud911@gmail.com>
2022-12-11 19:00:36 +0900

    :sparkles: (taeyoung/#69) jwt 토큰 디코드 함수 구현

---
+++ money-for-rabbit/src/utils/decodeJWT.js
@@ -0,0 +1,17 @@
+function decodeJWT(token) {
+  const base64Payload = token.split('.')[1];
+  const base64 = base64Payload.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split('')
+      .map((c) => {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+
+  return JSON.parse(jsonPayload);
+}
+
+export default decodeJWT;
