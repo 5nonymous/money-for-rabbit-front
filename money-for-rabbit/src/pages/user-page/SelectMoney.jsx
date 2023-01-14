ad9baa66d0566bda612caa3bc528c33d3c9ccd0a
taeyoung <rlaxodud911@gmail.com>
2023-01-14 22:06:22 +0900

    :iphone: (taeyoung/#160) 금액 선택 화면 최소 높이 지정 및 배경 색상 추가

--- money-for-rabbit/src/pages/user-page/SelectMoney.jsx
+++ money-for-rabbit/src/pages/user-page/SelectMoney.jsx
@@ -84,8 +84,10 @@ function SelectMoney() {
 export default SelectMoney;
 
 const wrapper = css`
+  background-color: ${common.color.white};
   width: 100%;
   height: 100%;
+  min-height: 720px;
 
   position: relative;
 `;
