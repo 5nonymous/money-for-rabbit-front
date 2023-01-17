3051872012b6f26c3cbd7abc30c7b5720c763a5d
halamlee <halam.lee.lami@gmail.com>
2023-01-17 14:08:24 +0900

    :construction: (halam/#161) api 요청했을 때 엑세스 토큰이 만료됐을 경우 리프레시 토큰으로 엑세스 토큰을 재발급 받아 다시 요청하게 함 - 테스트 이전

--- money-for-rabbit/src/utils/commonAxios.js
+++ money-for-rabbit/src/utils/commonAxios.js
@@ -10,10 +10,47 @@ commonAxios.interceptors.request.use((config) => {
   if (!token) {
     config.headers['Authorization'] = null;
     return config;
+  } else if (token && config.url === 'user/refresh') {
+    const refreshToken = localStorage.getItem('refreshToken');
+    config.headers['Authorization'] = 'Bearer ' + refreshToken;
+    return config;
   } else {
     config.headers['Authorization'] = 'Bearer ' + token;
     return config;
   }
 });
 
+commonAxios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const originalRequest = error.config;
+    console.log('originalRequest :>> ', originalRequest);
+    if (
+      error.response.status === 401 &&
+      error.response.data.error === '토큰이 만료되었습니다.'
+    ) {
+      commonAxios
+        .post('user/refresh')
+        .then((response) => {
+          const { access_token, refresh_token } = response.data;
+          localStorage.setItem('accessToken', access_token);
+          localStorage.setItem('refreshToken', refresh_token);
+          originalRequest.headers.authorization = `Bearer ${access_token}`;
+          return commonAxios(originalRequest);
+        })
+        .catch(() => {
+          // access token을 받아오지 못하는 오류 발생시 logout 처리
+          localStorage.removeItem('accessToken');
+          localStorage.removeItem('refreshToken');
+          window.location.href = '/';
+
+          return false;
+        });
+    }
+
+    return Promise.reject(error);
+  }
+);
 export default commonAxios;
